43a6052eeedac82c31362c937ea044155e32035e
RHISHINANDHINI <rhishinandhinia@gmail.com>
2025-07-24 19:23:49 +0530

    first commit

--- nova/settings.py
+++ nova/settings.py
@@ -74,11 +74,17 @@ WSGI_APPLICATION = 'nova.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "nova",
+        "USER": "postgres",
+        "PASSWORD": "rhishiarul",
+        "HOST": "127.0.0.1",
+        "PORT":"5432",
     }
 }
+
+
 
 
 # Password validation
@@ -115,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
